84fdc02b423ace749879319568657286a3426dd8
Jasmin Duong <jasmind009@gmail.com>
2025-05-16 12:10:21 -0400

    lab 3.1-5 done

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,59 @@
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ArrowFunctions from "./ArrowFunctions";
+import BooleanVariables from "./BooleanVariables";
+import Classes from "./Classes";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import Destructing from "./Destructing";
+import DestructingImports from "./DestructingImports";
+import FilterFunction from "./FilterFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import ForLoops from "./ForLoops";
+import FunctionDestructing from "./FunctionDestructing";
+import House from "./House";
+import IfElse from "./IfElse";
+import JsonStringify from "./JsonStringify";
+//import ImpliedReturn from "./ImpliedReturn";
+import LegacyFunctions from "./LegacyFunctions";
+import MapFunction from "./MapFunction";
+import SimpleArrays from "./SimpleArrays";
+import Spreading from "./Spreading";
+import Styles from "./Styles";
+import TemplateLiterals from "./TemplateLiterals";
+import TenaryOperator from "./TenaryOperator";
+import TodoList from "./TodoList";
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+
 export default function Lab3() {
   return (
-    <div>
+    <div id="wd-lab3">
       <h2>Lab 3</h2>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TenaryOperator />
+      <ConditionalOutputIfElse />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <TemplateLiterals />
+      <SimpleArrays />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+      <JsonStringify />
+      <House />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+      <Classes />
+      <Styles />
     </div>
   );
 }
