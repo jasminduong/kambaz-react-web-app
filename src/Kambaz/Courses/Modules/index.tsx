a91761623024ab57af83804a1dbdb1c5f43f7f28
Jasmin Duong <jasmind009@gmail.com>
2025-05-18 15:30:19 -0400

    3.8.7 done

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -3,150 +3,59 @@ import ModulesControls from "./ModulesControl";
 import ModuleControlButtons from "./ModuleControlButtons";
 import { BsGripVertical } from "react-icons/bs";
 import LessonControlButtons from "./LessonControlButtons";
+import { useParams } from "react-router";
+import * as db from "../../Database";
 
 /* Modules with all content */
 export default function Modules() {
+  {
+    /* retrieves course ID from the URL */
+  }
+  const { cid } = useParams();
+  const modules = db.modules;
+
   return (
     <div>
       <ModulesControls />
       <br />
       <br />
       <ListGroup className="rounded-0" id="wd-modules">
-        <ListGroup.Item className="wd-module p-0 mb-5 fs-6 border-lesson-outline">
-          <div className="wd-title p-3 ps-2 bg-gray-fill">
-            <BsGripVertical className="me-2 fs-3" /> Week 1{" "}
-            <ModuleControlButtons />
-          </div>
-          <ListGroup className="wd-lessons rounded-0">
-            <ListGroup.Item className="wd-lesson p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" /> LEARNING OBJECTIVES{" "}
-              <LessonControlButtons />
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Introduction to the course{" "}
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Learn what is Web Development{" "}
-            </ListGroup.Item>
-          </ListGroup>
-          <ListGroup className="wd-lessons rounded-0">
-            <ListGroup.Item className="wd-lesson p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" /> READING{" "}
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Full Stack Developer - Chapter 0 - Introduction{" "}
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Full Stack Developer - Chapter 1 - Creating User Interfaces{" "}
-            </ListGroup.Item>
-          </ListGroup>
-          <ListGroup className="wd-lessons rounded-0">
-            <ListGroup.Item className="wd-lesson p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" /> SLIDES{" "}
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Introduction to Web Development{" "}
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Creating an HTTP server with Node.js{" "}
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Creating a React Application{" "}
-            </ListGroup.Item>
-          </ListGroup>
-        </ListGroup.Item>
+        {modules
+          .filter((module: any) => module.course === cid)
+          .map((module: any, moduleIndex: number) => (
+            <ListGroup.Item
+              key={moduleIndex}
+              className="wd-module p-0 mb-5 fs-6 border-lesson-outline"
+            >
+              <div className="wd-title p-3 ps-2 bg-gray-fill">
+                <BsGripVertical className="me-2 fs-3" /> {module.name}{" "}
+                <ModuleControlButtons />
+              </div>
+              {module.lessons && (
+                <ListGroup className="wd-lessons rounded-0">
+                  {module.lessons.map((lesson: any) => (
+                    <div>
+                      <ListGroup.Item className="wd-lesson p-3 ps-1">
+                        <BsGripVertical className="me-2 fs-3" /> {lesson.name}{" "}
+                        <LessonControlButtons />
+                      </ListGroup.Item>
 
-        <ListGroup.Item className="wd-module p-0 mb-5 fs-6 border-lesson-outline">
-          <div className="wd-title p-3 ps-2 bg-gray-fill">
-            <BsGripVertical className="me-2 fs-3" /> Week 2{" "}
-            <ModuleControlButtons />
-          </div>
-          <ListGroup className="wd-lessons rounded-0">
-            <ListGroup.Item className="wd-lesson p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" /> LEARNING OBJECTIVES{" "}
-              <LessonControlButtons />
+                      {lesson["sub-lessons"] &&
+                        lesson["sub-lessons"].map(
+                          (subLesson: any) => (
+                            <ListGroup.Item
+                              className="wd-lesson p-3 ps-5"
+                            >
+                              {subLesson.name}{" "}
+                            </ListGroup.Item>
+                          )
+                        )}
+                    </div>
+                  ))}
+                </ListGroup>
+              )}
             </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Learn how to create user interfaces with HTML
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Deploy the assignment to Netlify
-            </ListGroup.Item>
-          </ListGroup>
-          <ListGroup className="wd-lessons rounded-0">
-            <ListGroup.Item className="wd-lesson p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" /> READING{" "}
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Full Stack Developer - Chapter 1 - Creating User Interfaces{" "}
-            </ListGroup.Item>
-          </ListGroup>
-          <ListGroup className="wd-lessons rounded-0">
-            <ListGroup.Item className="wd-lesson p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" /> SLIDES{" "}
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Formatting Web content with Headings and Paragraphs
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Formatting content with Lists and Tables
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Creating Web Forms
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Navigating with Anchors
-            </ListGroup.Item>
-          </ListGroup>
-        </ListGroup.Item>
-
-        <ListGroup.Item className="wd-module p-0 mb-5 fs-6 border-lesson-outline">
-          <div className="wd-title p-3 ps-2 bg-gray-fill">
-            <BsGripVertical className="me-2 fs-3" /> Week 3{" "}
-            <ModuleControlButtons />
-          </div>
-          <ListGroup className="wd-lessons rounded-0">
-            <ListGroup.Item className="wd-lesson p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" /> LEARNING OBJECTIVES{" "}
-              <LessonControlButtons />
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Learn how to style with CSS
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Styling with React Bootstrap CSS Library
-            </ListGroup.Item>
-          </ListGroup>
-          <ListGroup className="wd-lessons rounded-0">
-            <ListGroup.Item className="wd-lesson p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" /> READING{" "}
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Full Stack Developer - Chapter 2 - Styling Web Pages with CSS{" "}
-            </ListGroup.Item>
-          </ListGroup>
-          <ListGroup className="wd-lessons rounded-0">
-            <ListGroup.Item className="wd-lesson p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" /> SLIDES{" "}
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Introduction to Cascading Style Sheets
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Styling with Colors
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              The Box Model
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Size and Position
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">
-              Float
-            </ListGroup.Item>
-            <ListGroup.Item className="wd-lesson p-3 ps-5">Flex</ListGroup.Item>
-          </ListGroup>
-        </ListGroup.Item>
+          ))}
       </ListGroup>
     </div>
   );
