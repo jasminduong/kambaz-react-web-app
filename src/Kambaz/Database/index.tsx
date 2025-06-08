19ee5ef90e282bfd46806ccabf32f0d38fd88cb2
Jasmin Duong <jasmind009@gmail.com>
2025-06-08 13:27:52 -0400

    added modules json

--- src/Kambaz/Database/index.tsx
+++ src/Kambaz/Database/index.tsx
@@ -2,5 +2,6 @@ import assignments from "./assignments.json";
 import enrollments from "./enrollments.json";
 import users from "./users.json";
 import courses from "./courses.json";
+import modules from "./modules.json";
 
-export { assignments, enrollments, users, courses };
+export { assignments, enrollments, users, courses, modules };
